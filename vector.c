d9808e433e25080e2e56578e93398d8902600d2f
waynechen9099 <41244006s@gapps.ntnu.edu.tw>
2025-12-02 16:09:27 +0800

    SUB

--- vector.c
+++ vector.c
@@ -12,3 +12,14 @@ int* vADD(int *A,int *B,int size)
     }
     return C;
 }
+int* vSUB(int *A,int *B,int size)
+{
+    int *C = malloc(size*sizeof(int));
+
+    int i=0;
+    for(i=0;i<size;i++)
+    {
+        C[i] = A[i]-B[i];
+    }
+    return C;
+}
